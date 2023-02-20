bef70b96caf150e99cec7e953a5b8263c724095d
ndorrh <ndorrh@gmail.com>
2023-02-20 16:16:17 +0100

    Add Active Storage and set up Group model to upload category image and display it in view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -65,6 +65,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:name, :icon)
+      params.require(:group).permit(:name, :category_icon)
     end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+  has_one_attached :category_icon
   has_many :contract_groups, dependent: :destroy
   has_many :contracts, through: :contract_groups
 end
