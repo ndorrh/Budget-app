dee0eb4f1a1479e3c6c81a3facd26b398893354e
ndorrh <ndorrh@gmail.com>
2023-02-22 19:55:50 +0100

    Add unit test for groups and homes controller

---
+++ spec/requests/groups_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'rails_helper'
+
+RSpec.describe "Groups", type: :request do
+  before(:example) do
+    @user = User.create(name: "User 1", email: "test@gamil.com", password: 'password')
+    sign_in @user
+    @group = Group.create(name: "Group 1", icon: "icon1")
+  end
+
+describe 'GET /groups' do
+    it "Log's in user" do
+      get '/groups'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Get /groups url template' do
+      get '/groups'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'render index template)' do
+      get '/groups'
+      expect(response).to render_template(:index)
+    end
+
+    it 'render correct response body)' do
+      get '/groups'
+      expect(response.body).to include('Add category')
+    end
+  end
+
+  describe 'GET /groups/:id' do
+    it 'Get /groups/:id url template' do
+      get "/groups/#{@group.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'render show template)' do
+      get "/groups/#{@group.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'render correct response body)' do
+      get "/groups/#{@group.id}"
+      expect(response.body).to include('Group 1')
+    end
+  end
+end
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /homes" do
-    it "works! (now write some real specs)" do
+    it " get '/' " do
       get '/'
       expect(response).to have_http_status(200)
     end
