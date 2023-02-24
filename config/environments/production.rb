8bdbed7b4d143b25b29cf8e40deaf8d36d1a8e2d
ndorrh <ndorrh@gmail.com>
2023-02-24 16:30:24 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.serve_static_files = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
